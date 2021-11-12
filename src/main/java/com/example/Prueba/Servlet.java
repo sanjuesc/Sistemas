d2c56d645e2383cd479ed3d7d1ae85e110c97e08
Ander <asanjuan017@ikasle.ehu.eus>
2021-11-12 18:51:35 +0100

    ayuda

--- src/main/java/com/example/Prueba/Servlet.java
+++ src/main/java/com/example/Prueba/Servlet.java
@@ -4,11 +4,16 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 
 
 public class Servlet extends HttpServlet {
+String url = "jdbc:mysql://db:3306/example_db?connectTimeout=0&amp;socketTimeout=0&amp;autoReconnect=true";
+String username = "user";
+String password = "password";
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
 
@@ -19,16 +24,26 @@ public class Servlet extends HttpServlet {
             writer.println("<title>MyServlet.java:doGet(): Servlet code!</title>");
             writer.println("</head>");
             writer.println("<body>");
+            try{
+
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(url,username,password);
+                PreparedStatement st=con.prepareStatement("select date() from dual");
+                ResultSet rs = st.executeQuery();
+                Date fecha = rs.getDate(0);
+                writer.println("<h1>"+fecha+"</h1>");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
 
             writer.println("<h1>This is a simple java servlet.</h1>");
-
             writer.println("</body>");
             writer.println("</html>");
         }
     }
 
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         doGet(request, response);
     }
 }
