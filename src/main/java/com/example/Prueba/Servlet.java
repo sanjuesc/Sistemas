07be2584a584e18f98cea3877a32352948e7b61e
Ander <asanjuan017@ikasle.ehu.eus>
2021-11-14 17:46:41 +0100

    tus muertos

--- src/main/java/com/example/Prueba/Servlet.java
+++ src/main/java/com/example/Prueba/Servlet.java
@@ -23,8 +23,9 @@ Boolean done = false;
             }else{ //si tiene parametros miramos a ver si coinciden
                 String usuario = request.getParameter("uname");
                 String contra = request.getParameter("psw");
+                System.out.println("aaaaaaaaaaaaaa");
                 try{
-                    Class.forName("com.mysql.jdbc.Driver");
+                    Class.forName("com.mysql.cj.jdbc.Driver");
                     Connection con = DriverManager.getConnection(url,user,pass);
                     PreparedStatement st=con.prepareStatement("select name from user where name=? and pass = ?");
                     st.setString(1, usuario);
@@ -99,7 +100,7 @@ Boolean done = false;
                 String user = System.getenv("JDBC_USER");
                 String pass = System.getenv("JDBC_PASS");
                 String url = System.getenv("JDBC_URL");
-                Class.forName("com.mysql.jdbc.Driver");
+                Class.forName("com.mysql.cj.jdbc.Driver");
                 Connection con = DriverManager.getConnection(url,user,pass);
                 writer.write("estamos en el metodo<br>");
                 try {
