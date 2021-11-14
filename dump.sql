8391fc9141a7c795d4a5cfccd0822e250e84c15e
Ander <asanjuan017@ikasle.ehu.eus>
2021-11-14 13:40:42 +0100

    prueba cambiar collate

--- dump.sql
+++ dump.sql
@@ -23,7 +23,7 @@ CREATE TABLE `user` (
   `name` varchar(20) DEFAULT NULL,
   `pass` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
