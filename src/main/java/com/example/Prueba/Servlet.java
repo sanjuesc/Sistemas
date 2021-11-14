3fd2aba7b79a0f69ee07b5d45818cce7b8047a49
Ander <asanjuan017@ikasle.ehu.eus>
2021-11-14 20:07:14 +0100

    a e i o u b c

--- src/main/java/com/example/Prueba/Servlet.java
+++ src/main/java/com/example/Prueba/Servlet.java
@@ -42,6 +42,7 @@ Boolean done = false;
                 String contra = request.getParameter("psw");
                 System.out.println("aaaaaaaaaaaaaa");
                 try{
+                    System.out.println("venga a intenar loggear");
                     Class.forName("com.mysql.jdbc.Driver");
                     Connection con = DriverManager.getConnection(url,user,pass);
                     PreparedStatement st=con.prepareStatement("select name from user where name=? and pass = ?");
@@ -111,18 +112,14 @@ Boolean done = false;
                 String url = System.getenv("JDBC_URL");
                 Class.forName("com.mysql.jdbc.Driver");
                 Connection con = DriverManager.getConnection(url,user,pass);
-                writer.write("estamos en el metodo<br>");
                 try {
                     File f = new File("/usr/local/tomcat/dump.sql"); // source path is the absolute path of dumpfile.
-                    writer.write("file abierto<br>");
                     Statement stmt = con.createStatement();
                     BufferedReader bf = new BufferedReader(new FileReader(f));
                     String line = null,old="";
                     line = bf.readLine();
-                    writer.write("vamos al while<br>");
                     while (line != null) {
                         if(line.endsWith(";")){
-                            writer.write(old+line+"<br>");
                             stmt.executeUpdate(old+line);
                             old="";
                         }
