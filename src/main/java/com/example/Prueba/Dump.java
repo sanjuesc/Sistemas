da234584983c92743c227aff3089a38f0a11e6af
Ander <asanjuan017@ikasle.ehu.eus>
2021-11-20 10:59:36 +0100

    aey

--- src/main/java/com/example/Prueba/Dump.java
+++ src/main/java/com/example/Prueba/Dump.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Arrays;
 
 public class Dump extends HttpServlet {
 
@@ -22,10 +23,19 @@ public class Dump extends HttpServlet {
                     "  <meta http-equiv=\"refresh\" content=\"5; URL=/Prueba-1.0-SNAPSHOT/MyServlet\" />\n" +
                     "</head>\n" +
                     "<body>\n");
-
+            System.out.println("vamos a hacer el dump");
             //https://stackoverflow.com/questions/5711084/java-runtime-getruntime-getting-output-from-executing-a-command-line-program
-                    Runtime.getRuntime().exec("mysqldump  --column-statistics=0 -h db -u root -ppassword SISTEMAS > /usr/local/tomcat/misArchivos/dump.sql"); //no lo he probado con la linea esa, en teoria ahora funciona
-                    writer.println("  <p>If you are not redirected in five seconds, <a href=\"/Prueba-1.0-SNAPSHOT/MyServlet\">click here</a>.</p>\n" +
+            Process process =Runtime.getRuntime().exec("mysqldump  --column-statistics=0 -h db -u root -ppassword SISTEMAS > /usr/local/tomcat/misArchivos/dump.sql"); //no lo he probado con la linea esa, en teoria ahora funciona
+            InputStream is = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+
+            System.out.printf("Output is:");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            writer.println("  <p>If you are not redirected in five seconds, <a href=\"/Prueba-1.0-SNAPSHOT/MyServlet\">click here</a>.</p>\n" +
                     "</body>");
         }
     }
